35fbb3399a87821f0c76dd381e9a3f6ba3d92db6
Will Foster <wfoster@redhat.com>
2013-09-26 15:56:54 +0200

    provide an example for using glance, cinder and swift backed by gluster

---
+++ example-storage.pp
@@ -0,0 +1,26 @@
+# quickstack storage class
+class quickstack::storage {
+
+  class { 'gluster::server': }
+
+  volume { 'glance':
+    ensure         => present,
+    path           => '/srv/gluster/glance',
+    peers          => [ '192.168.0.2', '192.168.0.3', '192.168.0.4' ],
+    replica_count  => '3',
+  }
+
+  volume { 'cinder':
+    ensure         => present,
+    path           => '/srv/gluster/cinder',
+    peers          => [ '192.168.0.2', '192.168.0.3', '192.168.0.4' ],
+    replica_count  => '3',
+  }
+
+  volume { 'swift':
+    ensure         => present,
+    path           => '/srv/gluster/swift',
+    peers          => [ '192.168.0.2', '192.168.0.3', '192.168.0.4' ],
+    replica_count  => '3',
+  }
+}
