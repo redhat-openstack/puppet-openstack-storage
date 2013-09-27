9d722b97d81907bc7f35339d6e47ce2e0b0290fe
Gilles Dubreuil <gdubreui@redhat.com>
2013-09-27 13:45:34 +1000

    Added firewall rules for server

--- manifests/server.pp
+++ manifests/server.pp
@@ -13,4 +13,23 @@ class gluster::server (
   } -> service { $server_service_name:
     ensure => 'running',
   } 
+
+  firewall { '001 RPC incoming':
+    proto    => 'tcp',
+    dport    => [ '111' ],
+    action   => 'accept',
+  } 
+
+  firewall { '002 gluster daemon and management incoming':
+    proto    => 'tcp',
+    dport    => [ '24007', '24008' ],
+    action   => 'accept',
+  }
+
+  # 1 port per brick - We start with three
+  firewall { '001 gluster bricks incoming':
+    proto    => 'tcp',
+    dport    => [ '24009', '24010', '24011'],
+    action   => 'accept',
+  } 
 }
