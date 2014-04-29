c76c257004712443b86873787b63984944bbd284
Gilles Dubreuil <gilles@redhat.com>
2014-04-29 16:13:34 +1000

    Added a class offering same interface for 'cross compatibility' with new gluster module

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,6 +1,7 @@
 # OpenStack GlusterFS client
+# This is deprecated and replaced with gluster::mount::base class
 class gluster::client {
-    
+
     package { ['glusterfs-fuse', 'glusterfs-api']:
         ensure => installed,
     }
